35d65bd22811620a9ed5ed7a637661cb59ba8f38
samsun1309 <70271849+samsun1309@users.noreply.github.com>
2021-10-21 04:25:32 +0800

    json files

---
+++ js/vegalite_vis.js
@@ -0,0 +1,29 @@
+var vg_1 = "js/choropleth_map.json"; 
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var vg_2 = "js/line_chart.json"; 
+vegaEmbed("#line_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var vg_3 = "js/bar_chart.json"; 
+vegaEmbed("#bar_chart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);  
+
+var vg_4 = "js/timeline.json"; 
+vegaEmbed("#timeline", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);  
+
+var vg_5 = "js/scatter_plot.json"; 
+vegaEmbed("#scatter_plot", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);  
+
+var vg_6 = "js/scatter_plot2.json"; 
+vegaEmbed("#scatter_plot2", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);  
